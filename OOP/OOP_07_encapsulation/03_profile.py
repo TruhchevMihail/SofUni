e67e583c83e96a796577dfd98d962b170f6324cc
Mihail Truhchev <mishoiphoe@yahoo.com>
2025-07-02 23:18:12 +0300

    OOP classes and objects

---
+++ OOP/OOP_07_encapsulation/03_profile.py
@@ -0,0 +1,34 @@
+class Profile:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if len(value) < 5 or len(value) > 15:
+            raise ValueError("The username must be between 5 and 15 characters.")
+        self.__username = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        must_length = len(value) >= 8
+        have_upper = any([el for el in value if el.isupper()])
+        have_digit = any([el for el in value if el.isdigit()])
+
+        if not must_length or not have_upper or not have_digit:
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+        self.__password = value
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
+
+
+profile_with_valid_password = Profile('My_username', 'MyPassword1')
